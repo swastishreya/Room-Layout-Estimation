60c089f21463c9d55de0a7d1ebeb45ca9a52afef
swastishreya <mishra.swastishreya13@gmail.com>
2020-07-12 23:18:07 +0530

    modified train according to data specs

--- train.py
+++ train.py
@@ -1,9 +1,10 @@
 from Unet import UNet
-from dataloader import DocumentDeblurrDataset
-from metrics import PSNR, SSIM
+from dataloader import TwoDDataset
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from loss import dice_loss
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from torch.optim import lr_scheduler
@@ -16,27 +17,33 @@ import time
 
 class Trainer:
 
-    def __init__(self, model, optimizer, criterion, scheduler, num_epochs=25, batch_size=5):
+    def __init__(self, model, optimizer, scheduler, num_epochs=25, batch_size=1):
         gc.collect()
         self.model = model
         self.optimizer = optimizer
-        self.criterion = criterion
         self.scheduler = scheduler
         self.use_gpu = torch.cuda.is_available()
-        self.transform = transforms.Compose([transforms.ToTensor(),
-                                             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
         if self.use_gpu:
             self.model.cuda()
         self.num_epochs = num_epochs
         self.load_dataset(batch_size)
 
     def load_dataset(self, batch_size):
-        train_set = DocumentDeblurrDataset("/home/swasti/Documents/PE/BMVC_image_quality_train_data/",
-                                         "/home/swasti/Documents/PE/BMVC_image_quality_train_data/orig/",
-                                         transform=self.transform)
-        val_set = DocumentDeblurrDataset("/home/swasti/Documents/PE/BMVC_image_quality_val_data/",
-                                         "/home/swasti/Documents/PE/BMVC_image_quality_val_data/orig/",
-                                         transform=self.transform)
+        train_set = TwoDDataset("/home/swasti/Documents/hackathon/Room-Layout-Estimation/data/train/images/",
+                                "/home/swasti/Documents/hackathon/Room-Layout-Estimation/data/train/labels/",
+                                transform_image=transforms.Compose([transforms.ToTensor(),
+                                             transforms.Normalize((0.5, 0.5, 0.5, 0.5), (0.5, 0.5, 0.5, 0.5))]),
+                                transform_mask=transforms.Compose([transforms.ToTensor(),
+                                             transforms.Normalize(
+                                                 mean=[0.5],
+                                                    std=[0.5])]))
+        val_set = TwoDDataset("/home/swasti/Documents/hackathon/Room-Layout-Estimation/data/val/images/",
+                              "/home/swasti/Documents/hackathon/Room-Layout-Estimation/data/val/labels/",
+                              transform_image=transforms.Compose([transforms.ToTensor(),
+                                             transforms.Normalize((0.5, 0.5, 0.5, 0.5), (0.5, 0.5, 0.5, 0.5))]),
+                              transform_mask=transforms.Compose([transforms.ToTensor(),
+                                             transforms.Normalize(mean=[0.5],
+                                                                    std=[0.5])]))
         self.image_datasets = {
             'train': train_set, 'val': val_set
         }
@@ -46,11 +53,17 @@ class Trainer:
             'val': DataLoader(val_set, batch_size=batch_size, shuffle=True, num_workers=0)
         }
 
-    def calculate_loss(self, outputs, real_images, criterion, metrics):
-        loss = self.criterion(outputs, real_images)
-        # metrics['PSNR'] = PSNR(outputs, real_images)
-        metrics['SSIM'] += SSIM(outputs, real_images).data.cpu().numpy() * real_images.size(0)
-        metrics['Loss'] += loss.data.cpu().numpy() * real_images.size(0)
+    def calculate_loss(self, pred, target, metrics, bce_weight=0.5):
+        bce = F.binary_cross_entropy_with_logits(pred, target)
+
+        pred = torch.sigmoid(pred)
+        dice = dice_loss(pred, target)
+
+        loss = bce * bce_weight + dice * (1 - bce_weight)
+
+        metrics['bce'] += bce.data.cpu().numpy() * target.size(0)
+        metrics['dice'] += dice.data.cpu().numpy() * target.size(0)
+        metrics['loss'] += loss.data.cpu().numpy() * target.size(0)
         return loss
 
 
@@ -62,7 +75,7 @@ class Trainer:
         print("{}: {}".format(phase, ", ".join(outputs)))
 
     def train(self):
-        best_model_wts = copy.deepcopy(self.model.state_dict())
+        # best_model_wts = copy.deepcopy(self.model.state_dict())
         best_loss = 1e10
 
         for epoch in range(self.num_epochs):
@@ -81,10 +94,10 @@ class Trainer:
                 metrics = defaultdict(float)
                 epoch_samples = 0
 
-                for blurred_images, real_images in self.dataloaders[phase]:
+                for images, masks in self.dataloaders[phase]:
                     if self.use_gpu:
-                        blurred_images = blurred_images.cuda()
-                        real_images = real_images.cuda()
+                        images = images.cuda()
+                        masks = masks.cuda()
 
                     # zero the parameter gradients
                     self.optimizer.zero_grad()
@@ -92,8 +105,8 @@ class Trainer:
                     # forward
                     # track history if only in train
                     with torch.set_grad_enabled(phase == 'train'):
-                        outputs = self.model(blurred_images)
-                        loss = self.calculate_loss(outputs, real_images, self.criterion, metrics)
+                        outputs = self.model(images)
+                        loss = self.calculate_loss(outputs, masks, metrics)
 
                         # backward + optimize only if in training phase
                         if phase == 'train':
@@ -101,19 +114,19 @@ class Trainer:
                             self.optimizer.step()
 
                     # statistics
-                    epoch_samples += blurred_images.size(0)
+                    epoch_samples += images.size(0)
 
                 if phase == 'train':
                     self.scheduler.step()
 
                 self.print_metrics(metrics, epoch_samples, phase)
-                epoch_loss = metrics['loss'] / epoch_samples
+                # epoch_loss = metrics['loss'] / epoch_samples
 
                 # deep copy the model
-                if phase == 'val' and epoch_loss < best_loss:
-                    print("saving best model")
-                    best_loss = epoch_loss
-                    best_model_wts = copy.deepcopy(self.model.state_dict())
+                # if phase == 'val' and epoch_loss < best_loss:
+                #     print("saving best model")
+                #     best_loss = epoch_loss
+                #     best_model_wts = copy.deepcopy(self.model.state_dict())
 
             time_elapsed = time.time() - since
             print('{:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
@@ -121,14 +134,14 @@ class Trainer:
         print('Best val loss: {:4f}'.format(best_loss))
 
         # load best model weights
-        self.model.load_state_dict(best_model_wts)
+        # self.model.load_state_dict(best_model_wts)
         gc.collect()
 
 if __name__ == "__main__":
 
-    unet = UNet(in_channel=3,out_channel=3)
-    criterion = nn.L1Loss()
+    unet = UNet(in_channel=4,out_channel=1)
+    criterion = nn.L1Loss() # doesn't matter here 
     optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, unet.parameters()), lr=1e-3)
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
-    trainer = Trainer(unet,optimizer, criterion, exp_lr_scheduler)
+    trainer = Trainer(unet, optimizer, exp_lr_scheduler)
     trainer.train()
