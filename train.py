0a4ce152fe3f408063f755b2f8e94a601114b16d
swastishreya <mishra.swastishreya13@gmail.com>
2020-07-13 23:36:21 +0530

    updated training to save model and loss masks

--- train.py
+++ train.py
@@ -1,10 +1,11 @@
-from Unet import UNet
+# from Unet import UNet
+from MajdoorNet import MajdoorNet
 from dataloader import TwoDDataset
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from loss import dice_loss
+from loss import dice_loss, simple_distance_loss
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from torch.optim import lr_scheduler
@@ -35,34 +36,36 @@ class Trainer:
                                              transforms.Normalize((0.5, 0.5, 0.5, 0.5), (0.5, 0.5, 0.5, 0.5))]),
                                 transform_mask=transforms.Compose([transforms.ToTensor(),
                                              transforms.Normalize(
-                                                 mean=[0.5],
-                                                    std=[0.5])]))
+                                                 mean=[0],
+                                                    std=[1])]))
         val_set = TwoDDataset("/home/swasti/Documents/hackathon/Room-Layout-Estimation/data/val/images/",
                               "/home/swasti/Documents/hackathon/Room-Layout-Estimation/data/val/labels/",
                               transform_image=transforms.Compose([transforms.ToTensor(),
                                              transforms.Normalize((0.5, 0.5, 0.5, 0.5), (0.5, 0.5, 0.5, 0.5))]),
                               transform_mask=transforms.Compose([transforms.ToTensor(),
-                                             transforms.Normalize(mean=[0.5],
-                                                                    std=[0.5])]))
+                                             transforms.Normalize(mean=[0],
+                                                                    std=[1])]))
         self.image_datasets = {
             'train': train_set, 'val': val_set
         }
 
         self.dataloaders = {
-            'train': DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=0),
-            'val': DataLoader(val_set, batch_size=batch_size, shuffle=True, num_workers=0)
+            'train': DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=12),
+            'val': DataLoader(val_set, batch_size=batch_size, shuffle=True, num_workers=12)
         }
 
-    def calculate_loss(self, pred, target, metrics, bce_weight=0.5):
+    def calculate_loss(self, pred, target, coordinate_list, metrics, info, bce_weight=0):
         bce = F.binary_cross_entropy_with_logits(pred, target)
 
         pred = torch.sigmoid(pred)
-        dice = dice_loss(pred, target)
+        # dice = dice_loss(pred, target)
 
-        loss = bce * bce_weight + dice * (1 - bce_weight)
+        dist_loss = simple_distance_loss(pred, target, coordinate_list, self.use_gpu, info)
+
+        loss = bce * bce_weight + dist_loss * (1 - bce_weight)
 
         metrics['bce'] += bce.data.cpu().numpy() * target.size(0)
-        metrics['dice'] += dice.data.cpu().numpy() * target.size(0)
+        metrics['dist_loss'] += dist_loss.data.cpu().numpy() * target.size(0)
         metrics['loss'] += loss.data.cpu().numpy() * target.size(0)
         return loss
 
@@ -75,8 +78,9 @@ class Trainer:
         print("{}: {}".format(phase, ", ".join(outputs)))
 
     def train(self):
-        # best_model_wts = copy.deepcopy(self.model.state_dict())
+        best_model_wts = copy.deepcopy(self.model.state_dict())
         best_loss = 1e10
+        PATH = "model/majdoornet_dist_loss.pt"
 
         for epoch in range(self.num_epochs):
             print('Epoch {}/{}'.format(epoch, self.num_epochs - 1))
@@ -94,7 +98,12 @@ class Trainer:
                 metrics = defaultdict(float)
                 epoch_samples = 0
 
-                for images, masks in self.dataloaders[phase]:
+                info = { 'phase': phase, 'epoch': epoch }
+                if epoch != 0:
+                    f = open(info['phase']+'.npy', 'rb')
+                    info['array_file'] = f
+
+                for images, masks, coordinate_list in tqdm(self.dataloaders[phase]):
                     if self.use_gpu:
                         images = images.cuda()
                         masks = masks.cuda()
@@ -106,7 +115,7 @@ class Trainer:
                     # track history if only in train
                     with torch.set_grad_enabled(phase == 'train'):
                         outputs = self.model(images)
-                        loss = self.calculate_loss(outputs, masks, metrics)
+                        loss = self.calculate_loss(outputs, masks, coordinate_list, metrics, info)
 
                         # backward + optimize only if in training phase
                         if phase == 'train':
@@ -120,28 +129,33 @@ class Trainer:
                     self.scheduler.step()
 
                 self.print_metrics(metrics, epoch_samples, phase)
-                # epoch_loss = metrics['loss'] / epoch_samples
+                epoch_loss = metrics['loss'] / epoch_samples
 
                 # deep copy the model
-                # if phase == 'val' and epoch_loss < best_loss:
-                #     print("saving best model")
-                #     best_loss = epoch_loss
-                #     best_model_wts = copy.deepcopy(self.model.state_dict())
+                if phase == 'val' and epoch_loss < best_loss:
+                    print("saving best model")
+                    best_loss = epoch_loss
+                    best_model_wts = copy.deepcopy(self.model.state_dict())
+                    torch.save(self.model.state_dict(), PATH)
 
+                if epoch != 0:
+                    info['array_file'].close()
+                    
             time_elapsed = time.time() - since
             print('{:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
 
         print('Best val loss: {:4f}'.format(best_loss))
 
         # load best model weights
-        # self.model.load_state_dict(best_model_wts)
+        self.model.load_state_dict(best_model_wts)
         gc.collect()
 
 if __name__ == "__main__":
 
-    unet = UNet(in_channel=4,out_channel=1)
-    criterion = nn.L1Loss() # doesn't matter here 
-    optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, unet.parameters()), lr=1e-3)
+    # unet = UNet(in_channel=4,out_channel=1)
+    majdoorNet = MajdoorNet(4, 1)
+    # criterion = nn.L1Loss() # doesn't matter here 
+    optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, majdoorNet.parameters()), lr=1e-3)
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
-    trainer = Trainer(unet, optimizer, exp_lr_scheduler)
+    trainer = Trainer(majdoorNet, optimizer, exp_lr_scheduler)
     trainer.train()
