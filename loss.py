417e37d65826a971a908ee6b8c8f8255cef51a88
swastishreya <mishra.swastishreya13@gmail.com>
2020-07-17 02:10:12 +0530

    bugs squashed (but FIX ME)

--- loss.py
+++ loss.py
@@ -4,13 +4,13 @@ import torch.nn as nn
 import numpy as np
 import time
 
-def simple_distance_loss(pred, target, coordinate_list, use_gpu, info, umbrella_size=2):
+def simple_distance_loss(pred, target, coordinate_list, use_gpu, info, umbrella_size=100):
     loss_mask = np.zeros(target.shape, dtype='uint8')
 
     if info['epoch'] == 0:
         for target_y_cor, target_x_cor in coordinate_list:
-            for x_cor in range(min(0,target_x_cor-umbrella_size),min(512,target_x_cor+umbrella_size)):
-                for y_cor in range(min(0,target_y_cor-umbrella_size),min(512,target_y_cor+umbrella_size)):
+            for x_cor in range(max(0,target_x_cor-umbrella_size),min(512,target_x_cor+umbrella_size)):
+                for y_cor in range(max(0,target_y_cor-umbrella_size),min(1024,target_y_cor+umbrella_size)):
                     if x_cor != target_x_cor and y_cor != target_y_cor:
                         weight = 1./(abs(x_cor - target_x_cor) + abs(y_cor - target_y_cor))
                         if weight > loss_mask[0][0][x_cor][y_cor]:
@@ -30,7 +30,7 @@ def simple_distance_loss(pred, target, coordinate_list, use_gpu, info, umbrella_
     if use_gpu:
         loss_mask = loss_mask.cuda()
     
-    weighted_loss = (pred * loss_mask).mean()
+    weighted_loss = (1 - (pred * loss_mask)).mean()
     weighted_loss = weighted_loss/len(coordinate_list)
 
     return weighted_loss
